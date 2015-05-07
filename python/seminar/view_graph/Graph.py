3393e1d214dc841dec047cb423eb8803a61a97d6
SuzukiTomoyuki <dxfsrs1004@gmail.com>
2015-05-07 19:41:41 +0900

    t@N^®

--- python/seminar/view_graph/Graph.py
+++ python/seminar/view_graph/Graph.py
@@ -2,54 +2,35 @@ import pylab as pl
 import numpy as np
 from scipy import signal
 
-class Graph_type(object):
+class Graph_master(object):
 	def __init__(self,graph_type):
 		self.graph_type=graph_type
-		set_graph()
+		self.X = np.linspace(-2*np.pi, 2*np.pi, 256, endpoint=True)
+		self.Y=[]
 	def write_graph(self):
-		pass
+		pl.plot(self.X,self.Y)
+		pl.savefig(self.graph_type+'.png')
+		pl.show()
 
-class Graph(Graph_type):
+class Graph(Graph_master):
 	def __init__(self,gr_type):
 		super(Graph,self).__init__(gr_type)
-	def write_graph(self):	#overwrite
+	def graph(self):
 		if self.graph_type is 'nokogiri':
-
-			X = np.linspace(-2*np.pi, 2*np.pi, 256, endpoint=True)
-			Y = signal.sawtooth(X)
-			pl.plot(X,Y)
-			pl.savefig(self.graph_type+'.png')
-			pl.show()
-
+			self.Y = signal.sawtooth(self.X)
 		elif self.graph_type is 'cos':
-			X = np.linspace(-2*np.pi, 2*np.pi, 256, endpoint=True)
-			Y = np.cos(X)
-			pl.plot(X,Y)
-			pl.savefig(self.graph_type+'.png')
-			pl.show()
-
+			self.Y = np.cos(self.X)
 		elif self.graph_type is 'sin':
-			X = np.linspace(-2*np.pi, 2*np.pi, 256, endpoint=True)
-			Y = np.sin(X)
-			pl.plot(X,Y)
-			pl.savefig(self.graph_type+'.png')
-			pl.show()
-
+			self.Y = np.sin(self.X)
 		else: print 'faild'
 
-
-
-	def data_nokogiri(self):
-		pass
-
-
-def set_graph():
-	pl.xticks([-np.pi, -np.pi/2, 0, np.pi/2, np.pi],[r'$-\pi$', r'$-\pi/2$', r'$0$', r'$+\pi/2$', r'$+\pi$'])
-	pl.yticks([-1, 0, +1],[r'$-1$', r'$0$', r'$+1$'])
-	ax = pl.gca()
-	ax.spines['right'].set_color('none')
-	ax.spines['top'].set_color('none')
-	ax.xaxis.set_ticks_position('bottom')
-	ax.spines['bottom'].set_position(('data',0))
-	ax.yaxis.set_ticks_position('left')
-	ax.spines['left'].set_position(('data',0))
+	def set_graph(self):
+		pl.xticks([-np.pi, -np.pi/2, 0, np.pi/2, np.pi],[r'$-\pi$', r'$-\pi/2$', r'$0$', r'$+\pi/2$', r'$+\pi$'])
+		pl.yticks([-1, 0, +1],[r'$-1$', r'$0$', r'$+1$'])
+		ax = pl.gca()
+		ax.spines['right'].set_color('none')
+		ax.spines['top'].set_color('none')
+		ax.xaxis.set_ticks_position('bottom')
+		ax.spines['bottom'].set_position(('data',0))
+		ax.yaxis.set_ticks_position('left')
+		ax.spines['left'].set_position(('data',0))
--- python/seminar/view_graph/main.py
+++ python/seminar/view_graph/main.py
@@ -1,9 +1,18 @@
 import Graph
 
-if __name__=='__main__':
+def main():
 	graph1=Graph.Graph('nokogiri')
+	graph1.set_graph()
+	graph1.graph()
 	graph1.write_graph()
 	graph1.__init__('cos')
+	graph1.set_graph()
+	graph1.graph()
 	graph1.write_graph()
-	graph1.__init__('sin')
-	graph1.write_graph()
+	graph2=Graph.Graph('sin')
+	graph2.set_graph()
+	graph2.graph()
+	graph2.write_graph()
+
+if __name__=='__main__':
+	main()

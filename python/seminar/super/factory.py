50b331e53eb97b7fcface51e7c2f3988af65ba08
SuzukiTomoyuki <dxfsrs1004@gmail.com>
2015-05-14 19:20:27 +0900

    yakuzafactory

--- python/seminar/super/factory.py
+++ python/seminar/super/factory.py
@@ -1,5 +1,48 @@
+# coding:utf-8 
+
+class Item(object):
+	def __init__(self,caption):
+		self.caption = caption
+	def makeYakuza(self):
+		pass
+
+class Group(object):
+	def __init__(self,group_name):
+		self.group_name = group_name
+		self.content = []
+	def add(self,item):
+		self.content.append(item)
+	def output_file(self):
+		self.filename = self.group_name+"組.txt"
+		out = open(self.filename,"w")
+		out.write(str(self.makeYakuza()))
+		out.close()
+	def output_console(self):
+		print self.makeYakuza()
+	def makeYakuza(self):
+		pass
+
+class Name(Item):
+	def __init__(self,name):
+		super(Name,self).__init__(name)
+		self.name = name
+
+class Position(Item):
+	def __init__(self,position):
+		super(Position,self).__init__(position)
+		self.position = position
+		self.content = []
+	def add(self,item):
+		self.content.append(item)
+
 class Factory(object):
 	@classmethod
-	def getFacrtory(cls,classname):
-		module,kls = classname.rplit(".",1)
+	def getFactory(cls,classname):
+		module,kls = classname.rsplit(".",1)
 		return getattr(__import__(module),kls)()
+	def createGroup(self,group_name):
+		pass
+	def createName(self,name):
+		pass
+	def createPosition(self,position):
+		pass

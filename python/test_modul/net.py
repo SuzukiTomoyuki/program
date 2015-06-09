90c99f5d9c9d4d7d2a8c0d9b07242b6fa99a9c2d
SuzukiTomoyuki <dxfsrs1004@gmail.com>
2015-06-09 16:43:35 +0900

    add net_module

--- python/test_modul/net.py
+++ python/test_modul/net.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
+
 import networkx as nx
 import matplotlib.pylab as plt
+from numpy.random import *
 
 if __name__ == '__main__':
 	g = nx.Graph()
@@ -9,7 +12,15 @@ if __name__ == '__main__':
 	g.add_edge('node1','node2')
 	g.add_edges_from([('node1','node3'),('node2','node4')])
 
-	print g.nodes(),g.edges()
+	sample = nx.Graph()
 
-	nx.draw_networkx(g)
+	for i in xrange(0,60):
+		sample.add_node(i)
+
+	for i in xrange(0,60):
+		sample.add_edge(i,poisson(lam=30))
+	#print sample.edges()
+	try: print nx.shortest_path(sample, 30, 0)
+	except: print u"ぼっち"
+	nx.draw_networkx(sample)
 	plt.savefig("sample.png")

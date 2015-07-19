c7c63dc57e685a0f6c0f5ceb0b2e1b2f99cb3ef9
SuzukiTomoyuki <dxfsrs1004@gmail.com>
2015-07-20 07:54:40 +0900

    fix nicho hist

--- python/networkX/niconico_hist.py
+++ python/networkX/niconico_hist.py
@@ -8,15 +8,20 @@ from collections import Counter
 import csv
 import codecs
 import json
+import glob
 
 class NiconicoMovie(object):
 	def __init__(self, f):
-		line = f.readline().split("[")
-		nico_string = line[0].split(",") + line[1].split(",")
-		self.tags = []
-		for i in nico_string[15:len(nico_string)]:
-			if "tag" in i:
-				self.tags.append(i.split(":")[1].translate(string.maketrans("", ""), "}]"))
+		self.line = f.readline()
+		line_temp = self.line.split("[")
+		try:
+			nico_string = line_temp[0].split(",") + line_temp[1].split(",")
+			self.tags = []
+			for i in nico_string[15:len(nico_string)]:
+				if "tag" in i:
+					self.tags.append(i.split(":")[1].translate(string.maketrans("", ""), "}]"))
+		except:
+			pass
 
 
 class WordHistgram(NiconicoMovie):
@@ -37,30 +42,36 @@ class CustomFormat(csv.excel):
     quoting = csv.QUOTE_ALL
 
 if __name__ == '__main__':
-	file_path = '/Users/suzukitoshiyuki/program/complexnetwork/data/tcserv.nii.ac.jp/access/dxfsrs1004@gmail.com/2790ed0df755cd51/nicocomm/data/video/0000.dat'
-	# file_path = "C:\cygwin/files/tcserv.nii.ac.jp/access/dxfsrs1004@gmail.com/2790ed0df755cd51/nicocomm/data/video/0000.dat/0000.dat"
-	csvFile = codecs.open("./niconico.csv","w","shift_jis")
-	writer = csv.writer(csvFile, CustomFormat())
+	# file_path = '/Users/suzukitoshiyuki/program/complexnetwork/data/tcserv.nii.ac.jp/access/dxfsrs1004@gmail.com/2790ed0df755cd51/nicocomm/data/video/0000.dat'
+	csvFile = codecs.open("./niconico.csv","w","utf-8")
 	tags_append = []
-	G = nx.Graph()
-	f = open(file_path)
-	nico = WordHistgram(f)
-	for i in xrange(1000):
-	    nico.__init__(f)
-	    # nico.print_tags()
-	    nico.get_tags(tags_append)
+	tags_append2 = []
+	count = 0
+	path = "C:\cygwin/files/tcserv.nii.ac.jp/access/dxfsrs1004@gmail.com/2790ed0df755cd51/nicocomm/data/video/0000.dat/"
+	for file in glob.glob(path + "*.dat"):
+		# print file
+		# file_path = "C:\cygwin/files/tcserv.nii.ac.jp/access/dxfsrs1004@gmail.com/2790ed0df755cd51/nicocomm/data/video/0000.dat/0000.dat"
+	
+		f = open(file)
+		nico = WordHistgram(f)
+		while nico.line:
+		    nico.__init__(f)
+		    # nico.print_tags()
+		    if count < 500:
+		    	nico.get_tags(tags_append)
+		    else:
+		    	break
+		count+=1
 
 	counter = Counter(tags_append)
-	counter_append = []
 	for word, cnt in counter.most_common():
 	    # print "%s %s" % (word.decode("unicode-escape"), cnt)
-	    if cnt !=1 and word is not None :
+	    if cnt > 9:
 	    	# print "%s,%s" % (word.decode("unicode-escape"), cnt)
 	    	# writer.writerow((word.decode("unicode-escape"), cnt))
-	    	row = ("%s->%s" % (word, cnt))
-	    	print json.dumps(word)
-    		writer.writerow(row)
-	    	# counter_append.append("%s %s" % (word.decode("unicode-escape"), cnt))
+	    	print >> csvFile, json.loads(json.dumps(word)).decode("unicode-escape"),u" "+str(cnt)
+
+	f.close()
 
 
 
